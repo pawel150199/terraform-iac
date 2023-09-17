e3a6b2efc34dcb738adcb86202bfb3451d08d24e
pawel150199 <pawel.polski99@gmail.com>
2023-09-18 00:03:51 +0200

    fix typos

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.25.2"
+    }
+  }
+}
+
 provider "hcloud" {
-    token = var.hcloudtoken
+  token   = var.hcloud_token
 }
